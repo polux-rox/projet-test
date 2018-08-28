178bc74f18cfe3f955f4721a6f60d6fc4e70601e
Benjamin Besse <benjamin.besse02@gmail.com>
2018-08-28 16:26:17 +0200

    edit save element

--- src/components/common/Dialog.vue
+++ src/components/common/Dialog.vue
@@ -7,44 +7,47 @@
             <v-flex>
                 <v-container>
                     <v-layout wrap>
-                        <v-text-field
-                            v-model="editedItem.name"
-                            class="name"
-                            label="Name"
-                            clearable
-                            required/>
-                        <v-spacer/>
-                        <v-menu
-                            ref="menu"
-                            :close-on-content-click="false"
-                            v-model="menu"
-                            :nudge-right="40"
-                            :return-value.sync="date"
-                            lazy
-                            transition="scale-transition"
-                            offset-y
-                            full-width
-                            min-width="290px">
+                        <v-flex xs12>
                             <v-text-field
-                                slot="activator"
-                                v-model="editedItem.date"
-                                label="Picker in menu"
-                                prepend-icon="event"
-                                readonly/>
-                            <v-date-picker 
-                                v-model="editedItem.date"
-                                class="date"
-                                no-title 
-                                scrollable>
-                                <v-spacer/>
-                                <v-btn 
-                                    flat
-                                    @click="menu = false">Cancel</v-btn>
-                                <v-btn 
-                                    flat 
-                                    @click="$refs.menu.save(date)">OK</v-btn>
-                            </v-date-picker>
-                        </v-menu>
+                                v-model="editedItem.name"
+                                class="name"
+                                label="Name"
+                                clearable
+                                required/>
+                        </v-flex>
+                        <v-flex xs12>
+                            <v-menu
+                                ref="menu"
+                                :close-on-content-click="false"
+                                v-model="menu"
+                                :nudge-right="40"
+                                :return-value.sync="date"
+                                lazy
+                                transition="scale-transition"
+                                offset-y
+                                full-width
+                                min-width="290px">
+                                <v-text-field
+                                    slot="activator"
+                                    v-model="editedItem.date"
+                                    label="Picker in menu"
+                                    prepend-icon="event"
+                                    readonly/>
+                                <v-date-picker
+                                    v-model="editedItem.date"
+                                    class="date"
+                                    no-title
+                                    scrollable>
+                                    <v-spacer/>
+                                    <v-btn
+                                        flat
+                                        @click="menu = false">Cancel</v-btn>
+                                    <v-btn
+                                        flat
+                                        @click="$refs.menu.save(date)">OK</v-btn>
+                                </v-date-picker>
+                            </v-menu>
+                        </v-flex>
                     </v-layout>
                 </v-container>
             </v-flex>
@@ -71,14 +74,7 @@ export default {
         return {
             date        : null,
             menu        : false,
-            editedItem  : {
-                name    : '',
-                date    : '',
-            },
-            defaultItem : {
-                name    : '',
-                date    : '',
-            },
+            editedItem  : {},
             editedIndex : null,
         };
     },
@@ -100,17 +96,10 @@ export default {
         save() {
             if ((this.editedItem.name !== '') || (this.editedItem.date !== '')) {
                 this.$store.dispatch('task/save', this.editedItem);
+                this.editedItem = {};
                 console.log(this.$store.getters['task/getTasks']);
             }
         },
     },
 };
 </script>
-<style>
-  .name {
-    width: 405px;
-  }
-  .date {
-    width: 400px;
-  }
-</style>
